596abe1006b6bc933c604188220077fe63b3360f
Alex Hedlund <alex.hedlund@vincit.fi>
2022-06-14 10:02:02 +0300

    Add Plate editor

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      react: path.resolve('./node_modules/react'),
+      'react-dom': path.resolve('./node_modules/react-dom'),
+      // Alternatively:
+      // 'react/jsx-runtime.js': 'react/jsx-runtime',
+      // 'react/jsx-dev-runtime.js': 'react/jsx-dev-runtime',
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig
